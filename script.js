72cdcd16caeaf0b8208817606fd4256bc92dfd3c
sarthak-pokale <146321980+sarthak-pokale@users.noreply.github.com>
2025-01-16 20:33:57 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("menu-btn").addEventListener("click", function () {
-    const navLinks = document.querySelector(".nav-links");
-    if (navLinks.style.display === "flex") {
-        navLinks.style.display = "none";
-    } else {
-        navLinks.style.display = "flex";
-        navLinks.style.position = "absolute"; // Ensures dropdown stays separate
-    }
-});
